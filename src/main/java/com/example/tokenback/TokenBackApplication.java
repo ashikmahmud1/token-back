916a2df5732919012f6b07593fb5f06afacf5c1a
Ashik Mahmud <ashikmahmud137@gmail.com>
2020-03-01 23:07:19 +0600

    Spring boot project initialization with web,security,jpa and mysql driver

---
+++ src/main/java/com/example/tokenback/TokenBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenBackApplication.class, args);
+	}
+
+}
